b1deeb81ae8b4794c31b7f849e36ded28a57ae4a
Janak Soni <janak.soni@infostretch.com>
2018-09-20 14:01:44 -0700

    Enhancing POST Method to return correct HTTP Status Code and Location

--- src/main/java/com/practice/rest/webservices/restfulwebservices/user/UserResource.java
+++ src/main/java/com/practice/rest/webservices/restfulwebservices/user/UserResource.java
@@ -3,14 +3,17 @@
  */
 package com.practice.rest.webservices.restfulwebservices.user;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 /**
  * @author janak.soni
@@ -19,31 +22,29 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class UserResource {
-	
+
 	@Autowired
 	private UserDaoService userDaoService;
-	
-	// retrieveAllUsers
-	
+
 	@GetMapping("/users")
 	public List<User> retrieveAllUser() {
 		return userDaoService.findAll();
 	}
-	
-	// retrieveUser(int id)
-	
+
 	@GetMapping("/users/{id}")
 	public User retrieveUser(@PathVariable int id) {
 		return userDaoService.findOne(id);
 	}
-	
-	//CREATED
-	//input - details of user
-	// output - CREATED & Return created URI
-	
+
 	@PostMapping("/users")
-	public void createUser(@RequestBody User user) {
+	public ResponseEntity<Object> createUser(@RequestBody User user) {
 		User savedUser = userDaoService.save(user);
+
+		URI location = ServletUriComponentsBuilder
+			.fromCurrentRequest()
+			.path("/{id}")
+			.buildAndExpand(savedUser.getId()).toUri();
+		
+		return ResponseEntity.created(location).build();
 	}
-	
 }
