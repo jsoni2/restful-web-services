1747a0fc845793380b2fdd9fd360738cd6a5109a
Janak Soni <janak.soni@infostretch.com>
2018-09-21 11:54:50 -0700

    Implementing HATEOAS for RESTful Services

--- src/main/java/com/practice/rest/webservices/restfulwebservices/user/UserResource.java
+++ src/main/java/com/practice/rest/webservices/restfulwebservices/user/UserResource.java
@@ -3,12 +3,15 @@
  */
 package com.practice.rest.webservices.restfulwebservices.user;
 
+import static org.springframework.hateoas.mvc.ControllerLinkBuilder.*;
 import java.net.URI;
 import java.util.List;
 
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.hateoas.Resource;
+import org.springframework.hateoas.mvc.ControllerLinkBuilder;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -35,14 +38,21 @@ public class UserResource {
 	}
 
 	@GetMapping("/users/{id}")
-	public User retrieveUser(@PathVariable int id) {
+	public Resource<User> retrieveUser(@PathVariable int id) {
 		User user = userDaoService.findOne(id);
 		
 		if (user == null) {
 			throw new UserNotFoundException("id-" + id);
 		}
 		
-		return user;
+		Resource<User> resource = new Resource<User>(user);
+		
+		ControllerLinkBuilder linkTo = linkTo(methodOn(this.getClass()).retrieveAllUser());
+		
+		resource.add(linkTo.withRel("all-users"));
+		//HATEOAS
+		
+		return resource;
 	}
 	
 	@DeleteMapping("/users/{id}")
