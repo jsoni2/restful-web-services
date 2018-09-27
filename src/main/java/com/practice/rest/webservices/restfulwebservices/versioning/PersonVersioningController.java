89ef71b23f2a052a39e73e03aac94cc0469a6654
Janak Soni <janak.soni@infostretch.com>
2018-09-26 17:02:34 -0700

    Versioning RESTful Services - Basic Approach with URIs

---
+++ src/main/java/com/practice/rest/webservices/restfulwebservices/versioning/PersonVersioningController.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+package com.practice.rest.webservices.restfulwebservices.versioning;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author janak.soni
+ *
+ */
+
+@RestController
+public class PersonVersioningController {
+	
+	@GetMapping("v1/person")
+	public PersonV1 personV1() {
+		return new PersonV1("Bob Charlie");
+	}
+	
+	@GetMapping("v2/person")
+	public PersonV2 personV2() {
+		return new PersonV2(new Name("Jack","Smith"));
+	}
+
+}
