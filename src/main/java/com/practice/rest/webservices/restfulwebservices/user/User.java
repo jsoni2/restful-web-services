fc64324428804a14c204349a6d4878f0a58b752d
Janak Soni <janak.soni@infostretch.com>
2018-09-28 14:01:29 -0700

    Creating User Entity and some test data

--- src/main/java/com/practice/rest/webservices/restfulwebservices/user/User.java
+++ src/main/java/com/practice/rest/webservices/restfulwebservices/user/User.java
@@ -5,6 +5,9 @@ package com.practice.rest.webservices.restfulwebservices.user;
 
 import java.util.Date;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 import javax.validation.constraints.Past;
 import javax.validation.constraints.Size;
 
@@ -17,8 +20,11 @@ import io.swagger.annotations.ApiModelProperty;
  */
 
 @ApiModel(description="All details about the user.")
+@Entity
 public class User {
 	
+	@Id
+	@GeneratedValue
 	private Integer id;
 	
 	@Size(min=2, message="Name should have atleast 2 characters")
