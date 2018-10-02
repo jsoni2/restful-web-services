61949dcb6ca98590f4f37ce9df09fed731321e6b
Janak Soni <janak.soni@infostretch.com>
2018-10-02 10:04:18 -0700

    Implementing  a POST service to create a Post for a User

---
+++ src/main/java/com/practice/rest/webservices/restfulwebservices/user/PostRepository.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+package com.practice.rest.webservices.restfulwebservices.user;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+/**
+ * @author janak.soni
+ *
+ */
+
+@Repository
+public interface PostRepository extends JpaRepository<Post, Integer> {
+	
+
+}
